30c58481b9f68e91354b385f3ae26eeb6562442a
UjiagbeRex <rex.ujiagbe@gmail.com>
2024-03-13 09:52:12 +0100

    First commit

---
+++ Controllers/BankDataController.cs
@@ -0,0 +1,32 @@
+﻿using BankDataApi.Infrastructure.BankApi;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankDataApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankDataController : ControllerBase
+    {
+        private readonly IBankData _bankdata;
+
+        public BankDataController(IBankData bankdata)
+        {
+            _bankdata = bankdata;
+        }
+
+        //public IBankData Get_bankData()
+        //{
+        //    return _bankdata;
+        //}
+
+        [HttpGet]
+        [Route("Get")]
+
+        public async Task<IActionResult> GetNews()
+        {
+            await _bankdata.GetData();
+            return Ok();
+        }
+    }
+}
